80f2cfac3ae52c56b0998b438da388d2d8a066be
vucko23 <vucica93@yahoo.com>
2025-02-02 16:32:27 +0100

    Dodata funkc uklanjanja proizvod u ProductManager

--- product_manager.py
+++ product_manager.py
@@ -19,3 +19,11 @@ class ProductManager:
     def total_inventory_value(self):
         total = sum(product.price * product.quantity for product in self.products)
         return total
+    
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                print(f"Proizvod {product_name} je uklonjen.")
+                return
+        print(f"Proizvod {product_name} nije pronađen.")
