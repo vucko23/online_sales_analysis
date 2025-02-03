737a4d0798e7a9ddf9ddfdc603cd582b7b213ab4
vucko23 <vucica93@yahoo.com>
2025-02-03 22:05:27 +0100

    Save changes before merging add-cart-functionality branch

--- main.py
+++ main.py
@@ -6,19 +6,15 @@ from product_manager import ProductManager
 def main():
     pm = ProductManager()
 
-    product1 = Product("Laptop", 1000, 5)
-    product2 = Product("Smartphone", 500, 10)
-    product3 = Product("Tablet", 300, 7)
-    product4 = Product("Tv", 800, 3)
+    product1 = Product("Coffee", 10, 50)
+    product2 = Product("Tea", 20, 30)
+    product3 = Product("Banana", 30, 70)
+    product4 = Product("Apple", 50, 4)
 
     pm.add_product(product1)
     pm.add_product(product2)
     pm.add_product(product3)
     pm.add_product(product4)
-
-    print("Svi proizvodi:")
-    pm.display_all_products()
-    print("Ukupna vrijednost:", pm.total_inventory_value())
 
 if __name__ == "__main__":
     main()
